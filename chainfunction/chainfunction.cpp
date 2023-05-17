70ae14d711e16b00fc817c1e3bbaaa7c0cc00eba
safirasn21 <safirasn021@gmai.com>
2023-05-17 18:38:58 +0700

    menambahkan dan memanggil chain function

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -15,3 +15,11 @@ public:
 		return this->judul;
 	}
 } bukunya;
+
+int main()
+{
+	// bukunya.setJudul("Matematika");
+	// cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul(); //chai function calls
+	return 0;
+}
