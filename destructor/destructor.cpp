a64c2e0eeca5c5a665abacfb1ee6329b4b9d7b56
safirasn21 <safirasn021@gmai.com>
2023-05-17 18:53:41 +0700

    implementasi destructor class angka

---
+++ destructor/destructor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int); //constructor
+	~angka(); //destructor
+	void cetakData();
+	void isiData();
+};
+//Definisi member function
+angka::angka(int i){ //contructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
