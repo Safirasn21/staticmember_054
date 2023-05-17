d68e188d9c8e4122959f3eedfbe079077b314b9d
safirasn21 <safirasn021@gmai.com>
2023-05-17 19:01:16 +0700

    membuat objek baru dari class angka dengan menggunakan constractor angka(3)

--- destructor/destructor.cpp
+++ destructor/destructor.cpp
@@ -37,3 +37,11 @@ void angka::isiData() {
 	}
 	cout << endl;
 }
+
+int main() {
+	angka belajarcpp(3); //Constructor dipanggil
+	angka* ptrBelajarcpp = new angka(5); //constructor dipanggil
+	delete ptrBelajarcpp; //Destructor dipanggil
+
+	return 0;
+}//destructor dipanggil
